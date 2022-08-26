6d7ee950fd3c4dc917b67008cb20d4a711caa5e7
Felisware <107019287+felisware@users.noreply.github.com>
2022-08-26 20:25:55 +0700

    GUI revision of main window some widgets to public

--- Include/main_window.h
+++ Include/main_window.h
@@ -6,6 +6,30 @@
 
 class main_window : public wxFrame{
     public:
+    //make widgets accessible on main.cpp
+    
+    //page win update
+    wxStaticText *statusupdate;
+    wxCheckBox *advanced;
+    wxPanel *advanced_panel;
+    wxCheckBox *statustarget;
+    wxChoice *startuptype;
+    wxButton *action;
+    //page blocking firewall
+    wxListBox *list_blocked;
+    wxButton *add_btn;
+    wxButton *remove_btn;
+    wxButton *refresh_btn;
+    //custom DNS
+    wxChoice *interfaces;
+    wxRadioButton *defaultdns;
+    wxRadioButton *customdnsserver;
+    wxStaticBoxSizer *custombox;
+    wxTextCtrl *prefereddnsinput;
+    wxTextCtrl *alternatednsinput;
+    wxButton *apply;
+    wxButton *reset;
+
     main_window(const wxString &title, const wxPoint &pos, const wxSize &size);
 };
 
--- Src/main.cpp
+++ Src/main.cpp
@@ -12,9 +12,11 @@ wxIMPLEMENT_APP(program);
 bool program :: OnInit(){
     wxImage::AddHandler(new wxPNGHandler);
     main_window *main_form = new main_window("Easy Win Config", wxDefaultPosition, wxSize(300,400));
-    main_form->Show(true);
-    main_form->IsTopLevel();
+    main_form->Show();
     firewall_dialog *dialog_form = new firewall_dialog("Blocking Dialog", wxDefaultPosition, wxSize(320,325));
+    dialog_form->Raise();
     dialog_form->ShowModal();
+	dialog_form->Destroy();
+	dialog_form = 0;
     return 1;
 }
